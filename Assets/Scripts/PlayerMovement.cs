62b067af10944b26630cfa772aa4468efa7bb42b
Nelson Scarinci <scarincinelson@gmail.com>
2026-04-09 20:36:33 -0300

    Added camera rotation

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,32 +1,63 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
 public class PlayerMovement : MonoBehaviour
 {
     private CharacterController _cc;
-    private PlayerInput _pInput;
-    private Vector2 _inputValue;
+    private Vector2 _moveInputValue;
+    private Vector2 _lookInputValue;
     private Vector3 _movement;
-    [SerializeField] private int _speed;
-
+    [SerializeField] private Transform _cameraTransform;
+    [SerializeField] private int _moveSpeed;
+    [SerializeField] private float _cameraSpeed;
+    float _xRotation = 0f;
 
     private void Awake()
     {
         _cc = GetComponent<CharacterController>();
-        _pInput = GetComponent<PlayerInput>();
+    }
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 move = transform.right * _inputValue.x + transform.forward * _inputValue.y;
-        _movement = move * _speed * Time.deltaTime;
+        Vector3 move = transform.right * _moveInputValue.x + transform.forward * _moveInputValue.y;
+        _movement = move * _moveSpeed * Time.deltaTime;
 
         _cc.Move(_movement);
+
+
+        CameraMovement();
     }
 
     private void OnMove(InputValue input)
     {
-        _inputValue = input.Get<Vector2>();
+        _moveInputValue = input.Get<Vector2>();
+    }
+
+    private void OnLook(InputValue input)
+    {
+        _lookInputValue = input.Get<Vector2>();
+
+        Debug.Log($"El mouse se movió {_lookInputValue}");
+    }
+
+    private void CameraMovement()
+    {
+        float mouseX = _lookInputValue.x * _cameraSpeed * Time.deltaTime;
+        float mouseY = _lookInputValue.y * _cameraSpeed * Time.deltaTime;
+
+        _xRotation -= mouseY;
+        _xRotation = Mathf.Clamp(_xRotation, -80f, 80f);
+
+        _cameraTransform.localRotation = Quaternion.Euler(_xRotation, 0f, 0f);
+
+        transform.Rotate(Vector3.up * mouseX); //Rota el personaje segun la rotacion de la camara 
     }
 }
